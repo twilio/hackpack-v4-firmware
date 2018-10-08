6ea33db11f4d237efa858be543afba69ee1b18da
Andres <andres@enborra.com>
2018-10-07 19:05:29 -0700

    Adding node & npm to system basics install

--- bin/install.sh
+++ bin/install.sh
@@ -8,6 +8,8 @@ echo ""
 
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y git
+sudo apt-get install nodejs
+sudo apt-get install npm
 sudo apt-get install --no-install-recommends -y chromium-browser
 
 
