ce7e6b749e314cfaffcf6f344061e91e920dd628
Andres Krogh <andres@twilio.com>
2018-10-11 10:51:59 -0700

    Updated input drivers to correct inverted touchscreen cursor, formatting in master install.sh output, and formatting in readme

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,11 @@
 # Install basic system dependencies
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "(1 of 5) Installing system basics..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo apt-get update
@@ -18,7 +22,11 @@ sudo apt-get install --no-install-recommends -y npm
 # Run Driver install script
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "(2 of 5) Starting Hackpack Driver install..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo bash /home/pi/firmware/drivers/bin/install.sh
@@ -26,7 +34,11 @@ sudo bash /home/pi/firmware/drivers/bin/install.sh
 # Run CLI install script
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "(3 of 5) Starting Hackpack CLI install..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo bash /home/pi/firmware/cli/bin/install.sh
@@ -34,7 +46,11 @@ sudo bash /home/pi/firmware/cli/bin/install.sh
 # Run System API install script
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "(4 of 5) Installing SYSTEM API..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo bash /home/pi/firmware/system_api/bin/install.sh
@@ -42,7 +58,11 @@ sudo bash /home/pi/firmware/system_api/bin/install.sh
 # Run Kiosk install
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "(5 of 5) Installing Kiosk functionality..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo apt-get install -y libgtk-3-dev
@@ -57,7 +77,11 @@ sudo apt-get install --no-install-recommends -y matchbox
 sudo pip install pywebview[gtk3]
 
 echo ""
+echo "--------------------------------------------------"
+echo ""
 echo "Finished installing Hackpack v4. Rebooting..."
+echo ""
+echo "--------------------------------------------------"
 echo ""
 
 sudo shutdown -r now
