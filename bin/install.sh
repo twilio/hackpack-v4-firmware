ecb6dcea665c2ed61b795364a3c7b6643c94482a
Andres Krogh <andres@twilio.com>
2018-10-12 14:53:03 -0700

    fixing kiosk issue where write perms on creds files needed a chown during setup

--- bin/install.sh
+++ bin/install.sh
@@ -10,6 +10,14 @@ echo ""
 echo "--------------------------------------------------"
 echo ""
 
+sudo mkdir /home/pi/hp_tmp
+
+sudo touch /home/pi/hp_tmp/.hp_storage_
+sudo chown -R pi:pi /home/pi/hp_tmp/.hp_storage_
+
+sudo touch /home/pi/hp_tmp/.authtoken
+sudo chown -R pi:pi /home/pi/hp_tmp/.authtoken
+
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y git
 sudo apt-get install --no-install-recommends -y chromium-browser
